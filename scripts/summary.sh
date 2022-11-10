14389e21e7ac5cfa97b45d742cc1652d535b782f
code <code@flinteger.com>
2022-11-10 08:32:23 -0800

    Update script

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -2,13 +2,15 @@
 
 cd out
 
+total=0
 for f in *json;
 do
-  count=$(echo $(grep action $f | wc -l))
+  # count=$(echo $(grep action $f | wc -l))
+  count=$(jq '. | length' $f)
+  total=$(expr $total + $count)
   printf "%-22s: %s\n" $f $count
 done
 
 echo ""
-total=$(echo $(cat *.json | grep action | wc -l))
 printf "%-22s: %s\n" "Total rules" $total
 echo ""
