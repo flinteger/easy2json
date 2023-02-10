2757429a6991075b7dea5b628c32091126afade1
code <code@flinteger.com>
2023-02-10 15:13:00 -0800

    Add fallback top1m.domains

--- scripts/get_top1m.sh
+++ scripts/get_top1m.sh
@@ -3,5 +3,10 @@
 mkdir -p data
 rm -f top-1m.csv.zip top-1m.csv data/top1m.domains
 wget -q http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip
-unzip -q top-1m.csv.zip
-awk -F, '{ print $2 }' top-1m.csv > data/top1m.domains
+if [ -f top-1m.csv.zip ]; then
+    unzip -q top-1m.csv.zip
+    awk -F, '{ print $2 }' top-1m.csv > data/top1m.domains
+else
+    cp top1m.domains data/
+fi
+
